11f60b535075c2f72eb3f00542e5583e7141d47c
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-03-02 21:20:05 +0700

    fungsi3

---
+++ fungsi3.cpp
@@ -0,0 +1,28 @@
+//Library
+#include <iostream>
+using namespace std;
+
+//deklarasi variabel
+int p, l;
+
+//implementasi fungsi dan prosedur
+void input(){
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+
+int luasPersegiPanjang(){
+    return p * l;
+}
+
+void output(){
+    cout << "Hasilnya : " << luasPersegiPanjang();
+}
+
+int main(){ //mulai
+    input();
+    output();
+
+} //selesai
